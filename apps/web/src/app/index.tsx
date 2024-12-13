948734ed89ec4cab4481abf521e6408f5921a373
Vedant Chainani <vedantchainani1084@gmail.com>
2024-12-13 18:14:41 +0530

    chore: add start game logic

--- apps/web/src/app/index.tsx
+++ apps/web/src/app/index.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+
 import { createFileRoute } from '@tanstack/react-router';
 
 export const HomeComponent = () => {
+  const [gameStarted, setGameStarted] = useState(false);
   return (
-    <div className='!m-0 !p-0'>
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events -- safe to ignore
+    <div
+      className='!m-0 !p-0'
+      onClick={() => {
+        if (!gameStarted) {
+          setGameStarted(true);
+        }
+      }}
+    >
       <img
         alt='background'
         className='absolute h-screen w-full'
@@ -11,6 +22,13 @@ export const HomeComponent = () => {
       <div className='absolute top-[18%] right-1/2 z-[1] translate-x-1/2'>
         <img alt='logo' className='max-w-2xl' src='/logo.png' />
       </div>
+      {!gameStarted && (
+        <div className='absolute right-1/2 bottom-[20%] z-[1] translate-x-1/2'>
+          <div className='animate-pulse text-neutral-400'>
+            Click anywhere on the screen to start the game
+          </div>
+        </div>
+      )}
     </div>
   );
 };
